f4641700ffd3cb6f5fc6391a3123cb3edbadf4f4
Deepak Verma <deepakradialcode@gmail.com>
2024-12-14 15:43:02 +0530

    download lottie

--- src/common/CustomSection.jsx
+++ src/common/CustomSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import SubHeading from './SubHeading'
 
 const CustomSection = ({ myClass }) => {
   return (
-    <div className={`flex flex-wrap -mx-3`}>
+    <div className={`flex flex-wrap -mx-3 ${myClass} `}>
       <div className='w-1/2 px-3'>
-      
+          <SubHeading  />
       </div>
       <div className='w-1/2 px-3'></div>
     </div>
