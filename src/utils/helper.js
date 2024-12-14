416e06498b6bf401859f3f293bad32f5a3223686
Deepak Verma <deepakradialcode@gmail.com>
2024-12-14 12:59:20 +0530

    accordion arrray create

---
+++ src/utils/helper.js
@@ -0,0 +1,37 @@
+export const KEY_LIST = [
+    {
+        keyPoint: 'Let customers discover your content using AI-generated questions and answers',
+        title: 'Accordion Heading #one',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        colorfulPainting: '/assets/images/webp/colorful-painting.webp',
+        colorfulPaintingAlt: 'colorful-painting',
+        waterDropPainting: '/assets/images/webp/water-drop-painting.webp',
+        waterDropPaintingAlt: 'water-drop-painting'
+    },
+    {
+        keyPoint: 'Seamlessly integrated into the Semantic Search experience',
+        title: 'Accordion Heading #two',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        colorfulPainting: '/assets/images/webp/colorful-painting.webp',
+        colorfulPaintingAlt: 'colorful-painting',
+        waterDropPainting: '/assets/images/webp/water-drop-painting.webp',
+        waterDropPaintingAlt: 'water-drop-painting'
+    },
+    {
+        keyPoint: 'LLM-powered Question Answering',
+        title: 'Accordion Heading #three',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        colorfulPainting: '/assets/images/webp/colorful-painting.webp',
+        colorfulPaintingAlt: 'colorful-painting',
+        waterDropPainting: '/assets/images/webp/water-drop-painting.webp',
+        waterDropPaintingAlt: 'water-drop-painting'
+    },
+    {
+        title: 'Accordion Heading #four',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        colorfulPainting: '/assets/images/webp/colorful-painting.webp',
+        colorfulPaintingAlt: 'colorful-painting',
+        waterDropPainting: '/assets/images/webp/water-drop-painting.webp',
+        waterDropPaintingAlt: 'water-drop-painting'
+    }
+]
