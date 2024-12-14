98e733ae85bf8a2bcbafbb244f52561d58ea5e5b
Deepak Verma <deepakradialcode@gmail.com>
2024-12-14 12:48:19 +0530

    add image

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Key = () => {
   return (
-    <div>Key</div>
+    <div>
+      <div className='max-w-[1232px] mx-auto px-4'>
+        <div className='flex flex-wrap -mx-3'>
+          <div className='w-1/2 px-3'>
+            <img src="/assets/images/webp/working-girl.webp" alt="working-girl" className='h-[337px]' />
+          </div>
+          <div className='w-1/2 px-3'>
+            <div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
