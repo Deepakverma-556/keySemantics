58c749f8733c722bacbe82e7d9607657590b2bf1
Deepak Verma <deepakradialcode@gmail.com>
2024-12-16 14:12:30 +0530

    add image in footer

--- src/components/KeySemanticsForm.jsx
+++ src/components/KeySemanticsForm.jsx
@@ -6,7 +6,8 @@ const KeySemanticsForm = () => {
   return (
     <div className='bg-blue'>
       <div className='max-w-[1232px] mx-auto px-4 relative'>
-        <img src="/assets/images/webp/" alt="" />
+        <img src="/assets/images/webp/open-curly-braces.webp" alt="open-curly-braces" className='absolute h-[137px] top-0 left-0' />
+        <img src="/assets/images/webp/close-curly-braces.webp" alt="close-curly-braces" className='absolute h-[137px] bottom-0 right-0' />
         <SubHeading text='How does KeySemantics work' myClass='text-white text-center' />
         <Description text='Request a demo for your team and let us show you how KeySemantics can help your company' myClass='text-white text-center pt-2' />
         </div>
