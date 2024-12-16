18a049b94d6310bfb84f1c8fb36941722afc2a32
Deepak Verma <deepakradialcode@gmail.com>
2024-12-16 12:14:33 +0530

    data security section and federated section create

--- src/components/DataSecurity.jsx
+++ src/components/DataSecurity.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { DATA_SECURITY_LIST } from '../utils/helper'
-import CustomSection from '../common/CustomSection'
+import SubHeading from '../common/SubHeading'
+import CustomButton from '../common/CustomButton'
 
 const DataSecurity = () => {
   return (
-    <div className='max-w-[1232px] mx-auto px-4'>
-      <CustomSection text='Data Security and Hosting' myImage='/assets/images/webp/data-security.webp' myAlt='data-security' myClass='flex-col-reverse' />
-      {/* {
-        DATA_SECURITY_LIST.map((obj, i) => (
-          <ul>
-            <li className='list-disc'>{obj}</li>
-          </ul>
-        ))} */}
+    <div className='bg-lightGray pt-[148px] pb-[164px]'>
+      <div className='max-w-[1232px] mx-auto px-4'>
+        <div className='flex flex-wrap -mx-3'>
+          <div className='px-3 w-[51%]'>
+            <img src="/assets/images/webp/data-security.webp" alt="data-security" />
+          </div>
+          <div className='px-3 w-[49%]'>
+            <SubHeading text='Data Security and Hosting' />
+            {
+              DATA_SECURITY_LIST.map((obj, i) => (
+                <ul key={i} className='max-w-[552px] pl-6 pt-2'>
+                  <li className='list-disc'>{obj}</li>
+                </ul>
+              ))}
+            <CustomButton text='Get a demo' myClass='py-3 px-4 border border-blue text-blue hover:text-white after:bg-blue mt-6 max-sm:mt-5 max-md:mx-auto' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/HeadlessFederated.jsx
+++ src/components/HeadlessFederated.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import CustomSection from '../common/CustomSection'
 import { HEADLESS_LIST } from '../utils/helper'
 import CustomButton from '../common/CustomButton'
+import SubHeading from '../common/SubHeading'
 
 const HeadlessFederated = () => {
   return (
-    <div className='max-w-[1232px] mx-auto px-4'>
-      <CustomSection text='Headless federated search' myImage='/assets/images/webp/federated-search.webp' myAlt='federated-search' />
-      {/* {
-        HEADLESS_LIST.map((obj, i) => (
-          <ul>
-            <li className='list-disc'>{obj}</li>
-          </ul>
-        ))} */}
+    <div className='bg-lightGray'>
+      <div className='max-w-[1232px] mx-auto px-4'>
+        <div className='flex flex-wrap -mx-3'>
+          <div className='px-3 w-[49%]'>
+            <SubHeading text='Headless federated search' />
+            {
+              HEADLESS_LIST.map((obj, i) => (
+                <ul key={i} className='pl-6 max-w-[552px] pt-2'>
+                  <li className='list-disc'>{obj}</li>
+                </ul>
+              ))}
+            <CustomButton text='Get a demo' myClass='py-3 px-4 border border-blue text-blue hover:text-white after:bg-blue mt-6 max-sm:mt-5 max-md:mx-auto' />
+          </div>
+          <div className='px-3 w-[51%]'>
+            <img src="/assets/images/webp/federated-search.webp" alt="federated-search" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
